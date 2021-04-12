a872afb479ced4d69db31e706161ee7c26e32e28
Combatd <combatdstar@gmail.com>
2021-04-12 13:51:39 -0700

    Declare Policy._instance

--- SingletonPattern/Singleton/Policy.cs
+++ SingletonPattern/Singleton/Policy.cs
@@ -6,8 +6,29 @@ using System.Threading.Tasks;
 
 namespace Singleton
 {
+    
     public class Policy
     {
+        private static Policy _instance;
+        public static Policy Instance
+        {
+            get
+            {
+                if(_instance == null) // make sure there is only once instance of Policy
+                {
+                    return new Policy();
+                } else
+                {
+                    return _instance;
+                }
+                
+            }
+        }
+        public Policy()
+        {
+
+        }
+
         private int Id { get; set; } = 123;
         private string Insured { get; set; } = "John Roy";
 
