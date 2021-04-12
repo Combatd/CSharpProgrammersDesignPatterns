65a7c6058f3eb7332a11a098b8355330f67c11e0
Combatd <combatdstar@gmail.com>
2021-04-11 21:27:19 -0700

    Implement AbstractFactory for Concrete LoanAccount and SavingsAccount

---
+++ CitiAbstractFactory/Interfaces/Common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    // Abstract Factory
+    /*
+     * Start off by creating an abstract class.
+     * 
+     * Operations:
+     * Create Savings Account
+     * Create Loan Account
+     */
+    public abstract class ICreditUnionFactory
+    {
+        public abstract ISavingsAccount CreateSavingsAccount();
+        public abstract ILoanAccount CreateLoanAccount();
+    }
+
+    /*
+    * Next, create an abstraction for the products, 
+    * ILoanAccount and ISavingsAccount will be implemented
+    */
+
+    // Abstract Product A
+    public interface ILoanAccount { }
+
+    // Abstract Product B
+    public interface ISavingsAccount { }
+}
