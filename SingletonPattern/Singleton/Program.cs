59fafaf392fbc6213b33c40e3861f6a0658f680b
Combatd <combatdstar@gmail.com>
2021-04-11 22:33:47 -0700

    Comment description of Singelton

--- SingletonPattern/Singleton/Program.cs
+++ SingletonPattern/Singleton/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Singleton
 {
+    /* Singleton Pattern
+     * 
+     * Ensures a class has only one instance, and provides
+     * a global point of access to it.
+     * 
+     * Eliminates the option of instantiating more than one of the same object.
+     */
+
     public class Program
     {
         static void Main(string[] args)
