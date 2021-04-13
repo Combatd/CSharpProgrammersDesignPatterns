4f313a641816b9f20f486a18a0fada59d15d1826
Combatd <combatdstar@gmail.com>
2021-04-12 21:24:21 -0700

    Create Iterator Project

---
+++ IteratorPattern/Iterator/Program.cs
@@ -0,0 +1,45 @@
+﻿using Iterator.Aggregate;
+using Iterator.Iterator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    /* The Iterator Pattern provides a way to access the elements of
+     * an aggregate object sequentially without exposing its underlying
+     * representation.
+     * 
+     * It provides a way to access and iterate through collections
+     * in the same way, regardless of type.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INewspaper nyt = new NYPaper();
+            INewspaper lat = new LAPaper();
+
+            IIterator nypIterator = nyt.CreateIterator();
+            IIterator lapIterator = lat.CreateIterator();
+
+            Console.WriteLine("--------   NYPaper");
+            PrintReporters(nypIterator);
+
+            Console.WriteLine("--------   LAPaper");
+            PrintReporters(lapIterator);
+
+            Console.ReadLine();
+        }
+
+        static void PrintReporters(IIterator iterator) {
+            iterator.First();
+            while(!iterator.IsDone()){
+                Console.WriteLine(iterator.Next());
+            }
+        }
+    }
+}
